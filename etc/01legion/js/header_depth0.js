391efda66db2012a02688fcef12eef9f1fb1cdb0
seihareika <133826539+seihareika@users.noreply.github.com>
2023-06-22 00:28:06 +0900

    Add files via upload

---
+++ etc/01legion/js/header_depth0.js
@@ -0,0 +1,25 @@
+document.write(`
+<header>
+<h1></h1><h2></h2>
+<div class="openbtn"><span></span><span></span><span></span></div>
+<nav id="g-nav">
+    <div id="g-nav-list"><!--ナビの数が増えた場合縦スクロールするためのdiv※不要なら削除-->
+        <ul>
+            <li><a href="https://seihareika.github.io/seihareikalab/etc/01legion/index.html"><h1>TOP</h1></a></li>
+            <br><hr><br>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+            <li><a href="/"><h3></h3></a></li>
+        </ul>
+    </div>
+</nav>
+<a href="#" class="page_top_btn">aaaa</a>
+</header>
+`);
